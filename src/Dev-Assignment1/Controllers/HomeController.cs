6edf61b4044cdd5b6854ace70521c94b92c2c947
Jessel94 <jesselakerveld@hotmail.com>
2016-10-21 18:49:42 +0200

    Updated the employee page to use a model list

--- src/Dev-Assignment1/Controllers/HomeController.cs
+++ src/Dev-Assignment1/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using Dev_Assignment1.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Dev_Assignment1.Controllers
@@ -11,6 +9,30 @@ namespace Dev_Assignment1.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Employee()
+        {
+            var employeeList = new List<Employee>
+            {
+                new Employee
+                {
+                    IdEmployee = 1,
+                    Bsn = "1234",
+                    Name = "Henk",
+                    Surname = "Jan"
+                },
+                new Employee
+                {
+                    IdEmployee = 2,
+                    Bsn = "5678",
+                    Name = "Jaap",
+                    Surname = "Worst"
+                }
+            };
+
+
+            return View(employeeList);
         }
 
         public IActionResult About()
---
+++ src/Dev-Assignment1/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dev_Assignment1.Models
+{
+    public class Employee
+    {
+        public int IdEmployee { get; set; }
+
+        public string Bsn { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
