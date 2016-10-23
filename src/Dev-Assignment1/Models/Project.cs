81e3b986a4edad2d16f919c13a0716767e290533
Jessel94 <jesselakerveld@hotmail.com>
2016-10-23 14:27:56 +0200

    Added project model

---
+++ src/Dev-Assignment1/Models/Project.cs
@@ -0,0 +1,18 @@
+﻿namespace Dev_Assignment1.Models
+{
+    public class Project
+    {
+        public Project(int idProject, string budget, decimal hours)
+        {
+            IdProject = idProject;
+            Budget = budget;
+            Hours = hours;
+        }
+
+        public int IdProject { get; }
+
+        public string Budget { get; }
+
+        public decimal Hours { get; }
+    }
+}
